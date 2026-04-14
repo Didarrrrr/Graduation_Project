7f803ff4f846b6de27bb6b98939fe5e47de4ec49
Didar <diiid4rrr@gmail.com>
2026-04-14 20:46:38 +0300

    hosting update

--- image_forgery_detection/settings.py
+++ image_forgery_detection/settings.py
@@ -32,8 +32,17 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://graduationproject-production-5744.up.railway.app',
+]
+
+ALLOWED_HOSTS = [
+    'graduationproject-production-5744.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 
